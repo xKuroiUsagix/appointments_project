4c72b2ed5121b99623b1ae76594fd8365ab46bc8
xKuroiUsagix <povoroznikvitalii@gmail.com>
2022-07-06 15:54:35 +0300

    chore: remove unnecessary imports

--- apps/client_api/models.py
+++ apps/client_api/models.py
@@ -2,8 +2,6 @@ from django.db import models
 from django.utils.translation import gettext_lazy as _
 from django.contrib.auth import get_user_model
 
-from appointments_project import settings
-
 
 User = get_user_model()
 
--- apps/specialist_api/models.py
+++ apps/specialist_api/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from django.utils import timezone
 from django.utils.timezone import datetime
 from django.contrib.auth import get_user_model
 from django.utils.translation import gettext_lazy as _
