c0c828ec07bbf8d3bd59008e2b0311ba201c43c7
xKuroiUsagix <povoroznikvitalii@gmail.com>
2022-07-16 02:28:05 +0300

    chore: change imports ordering

--- appointments_project/settings.py
+++ appointments_project/settings.py
@@ -1,9 +1,8 @@
 from datetime import timedelta
+from pathlib import Path
 import json
 import sys
 import os
-
-from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
